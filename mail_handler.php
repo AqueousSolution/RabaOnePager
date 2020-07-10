1e877de69ae7138cf3ad052f6bbfcffa6a170c16
Idahosa Akioya <akioyaidahosa@yahoo.com>
2020-07-10 16:34:45 +0100

    testingphp

---
+++ mail_handler.php
@@ -0,0 +1,18 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "idahosa@spinlet.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $subject = $_POST['subject'];
+    $subject2 = "Copy of your Enquiry";
+    $message =  " A new user submitted the following enquiry on the website :" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    }
+?>
