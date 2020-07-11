1996b4435f567041e460f570d316d2c7cd7eeff1
Idahosa Akioya <akioyaidahosa@yahoo.com>
2020-07-11 03:32:24 +0100

    netlifyforms

--- mail_handler.php
+++ mail_handler.php
@@ -11,7 +11,7 @@ if(isset($_POST['submit'])){
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    echo "Mail Sent. Thank you  we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // You cannot use header and echo together. It's one or the other.
     }
